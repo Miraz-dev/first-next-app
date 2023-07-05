390272201a5e2c15d8915262e4907953b72b0e4e
Miraz Farid <miraz.officialx@gmail.com>
2023-07-06 02:46:36 +0600

    Learned about data fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,46 +3,46 @@ import styles from "./page.module.css"
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Blog = () => {
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", { cache: 'no-store' });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+
+
+const Blog = async () => {
+
+
+  const data = await getData();
+
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
+      {data.map(
+        (item) => (
+          <Link href="/blog/testId" className={styles.container} key={item.id}>
 
-        <div className={styles.imgContainer}>
-          <Image 
-            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.imgage}
-          />
-        </div>
+            <div className={styles.imgContainer}>
+              <Image
+                src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+                alt=""
+                width={400}
+                height={250}
+                className={styles.Image}
+              />
+            </div>
 
-        <div className={styles.content}>
-          <h1 className={styles.title}>TestTitle</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-        
-      </Link>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.desc}>{item.body}</p>
+            </div>
 
-      <Link href="/blog/testId" className={styles.container}>
-
-        <div className={styles.imgContainer}>
-          <Image 
-            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.imgage}
-          />
-        </div>
-
-        <div className={styles.content}>
-          <h1 className={styles.title}>TestTitle</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-
-      </Link>
+          </Link>
+        )
+      )}
     </div>
   )
 }
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,31 +2,49 @@ import React from 'react'
 import styles from "./page.module.css"
 import Button from '@/components/Button/Button'
 import Image from 'next/image'
+import { items } from './data'
+import { notFound } from 'next/navigation'
+
+const getData = (cat) => {
+  const data = items[cat];
+
+  if(data){
+    return data
+  }
+
+  return notFound()
+}
 
 const Category = ({ params }) => {
-  console.log(params)
+  const data = getData(params.category);
+  console.log(params);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
 
-      <div className={styles.item}>
+      {data.map(
+        (items) => (
+          <div className={styles.item} key={items.id}>
 
-        <div className={styles.content}>
-          <h1 className={styles.title}>Creative Content</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
-        </div>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{items.title}</h1>
+              <p className={styles.desc}>{items.desc}</p>
+              <Button text="See More" url="#" />
+            </div>
 
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-            fill={true}
-            alt=""
-          />
-        </div>
+            <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                src={items.image}
+                fill={true}
+                alt=""
+              />
+            </div>
 
-      </div>
+          </div>
+        )
+      )}
 
     </div>
   )
